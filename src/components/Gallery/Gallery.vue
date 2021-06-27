a3259f63c190ac31dae91de502d194824ff85938
chobrandone <chobrandone4@gmail.com>
2021-06-27 01:51:21 +0100

    last commit

---
+++ src/components/Gallery/Gallery.vue
@@ -0,0 +1,55 @@
+<template >
+    <div>
+<div class="container">
+	<div class="row">
+		<h2>Photo Gallery Preview with large and small images align</h2>
+	</div>
+	
+	<div class="row img-thumbnails">
+	    <div class="col-md-6">
+	        <a href="#">
+	            <img src="http://lorempixel.com/600/600/nature/1/" class="img-thumbnail">
+	        </a>
+	    </div>
+	    <div class="col-md-6">
+	        <a href="#">
+	            <img src="http://lorempixel.com/600/600/nature/2/" class="img-thumbnail">
+	        </a>
+	    </div>
+	    <div class="col-md-4">
+	        <a href="#">
+	            <img src="http://lorempixel.com/600/600/nature/3/" class="img-thumbnail">
+	        </a>
+	    </div>
+	    <div class="col-md-4">
+	        <a href="#">
+	            <img src="http://lorempixel.com/600/600/nature/4/" class="img-thumbnail">
+	        </a>
+	    </div>
+	    <div class="col-md-4">
+	        <a href="#">
+	            <img src="http://lorempixel.com/600/600/nature/5/" class="img-thumbnail">
+	        </a>
+	    </div>
+	</div>
+	
+</div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{ 
+
+          openimage()
+    }
+}
+</script>
+<style scoped>
+
+    
+</style>
