a53d5bf83143e333cd1157f7d252e4354df0ce4d
DFRICHARD <54338939+DFRICHARD@users.noreply.github.com>
2021-07-07 20:54:52 +0200

    update Gallery.vue

--- src/components/Gallery/Gallery.vue
+++ src/components/Gallery/Gallery.vue
@@ -1,55 +1,83 @@
-<template >
+<template>
+<div>
+
     <div>
-<div class="container">
-	<div class="row">
-		<h2>Photo Gallery Preview with large and small images align</h2>
-	</div>
-	
-	<div class="row img-thumbnails">
-	    <div class="col-md-6">
-	        <a href="#">
-	            <img src="http://lorempixel.com/600/600/nature/1/" class="img-thumbnail">
-	        </a>
-	    </div>
-	    <div class="col-md-6">
-	        <a href="#">
-	            <img src="http://lorempixel.com/600/600/nature/2/" class="img-thumbnail">
-	        </a>
-	    </div>
-	    <div class="col-md-4">
-	        <a href="#">
-	            <img src="http://lorempixel.com/600/600/nature/3/" class="img-thumbnail">
-	        </a>
-	    </div>
-	    <div class="col-md-4">
-	        <a href="#">
-	            <img src="http://lorempixel.com/600/600/nature/4/" class="img-thumbnail">
-	        </a>
-	    </div>
-	    <div class="col-md-4">
-	        <a href="#">
-	            <img src="http://lorempixel.com/600/600/nature/5/" class="img-thumbnail">
-	        </a>
-	    </div>
-	</div>
-	
-</div>
+        <div class="">
+
+            <Head />
+        </div>
     </div>
+    <div class="">
+
+        <Gallery />
+    </div>
+
+    <div class="container mt-5 mb-5">
+
+        <div class="row img-thumbnails">
+            <div class="col-md-6 mb-3">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G1.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+            <div class="col-md-6 mb-3">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G2.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+            <div class="col-md-4 mb-4">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G3.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+			
+            <div class="col-md-4 mb-4">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G4.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+            <div class="col-md-4 mb-4">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G6.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="#">
+                    <img src="../../assets/img/gallery/G7.jpeg" class="img-thumbnail">
+                </a>
+            </div>
+        </div>
+
+    </div>
+    <Footer />
+</div>
 </template>
+
 <script>
+import Head from '../Header/Header.vue'
+import Gallery from './galleryhero.vue'
+import Footer from '../Footer/Footer.vue'
+// import Images from '../../assets/img/gallery/G1.jpeg'
 export default {
     data() {
         return {
-            
+
         }
     },
-    methods:{ 
-
-          openimage()
+    components: {
+        Gallery,
+        Head,
+        Footer
     }
+
 }
 </script>
-<style scoped>
 
-    
+<style scoped>
+/* .col-md-4{ height: 200PX;} */
 </style>
