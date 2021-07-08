e64f8a1be1a4548b50b85699137297dec36b7129
chobrandone <chobrandone4@gmail.com>
2021-07-08 10:08:18 +0100

    last commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 
 import Home from '../components/Home/Home.vue'
 import Blog from '../components/Blog/Blog.vue'
-import BlogHero from '../components/Blog/BlogHero.vue'
-import Blog from '../components/Blog/OurBlog.vue'
+
 import Contact from '../components/Contact/Contact.vue'
 import Aboutus from '../components/About/About.vue';
 import Download from '../components/Download/Download.vue'
 import Service from '../components/OurServices/Services.vue'
-import Gallery from '../components/Gallery/gallery.vue'
+import Gallery from '../components/Gallery/Gallery.vue'
 import Team from '../components/OurTeam/Team.vue'
 import Testimonials from "../components/Testimolials/Testimonials.vue"
 
@@ -29,11 +28,7 @@ const routes =[
         name:'Blog',
         component: Blog
     },
-    {
-        path :'/blog/:id',
-        name:'single-Blog',
-        component: BlogHero
-    },
+   
     {
         path :'/about',
         name:'Aboutus',
